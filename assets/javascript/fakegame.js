a636a297be6ddd8c98dda6bad19c4734b79df98c
Shane Drosi <ShaneDrosi@Joshuas-MacBook-Air.local>
2018-02-14 15:10:27 -0800

    a meager commit

---
+++ assets/javascript/fakegame.js
@@ -0,0 +1,46 @@
+//variables
+
+//answer word
+
+//VARIABLES
+var myArray = ["fire", "crackle", "hiking", "mountain", "desert"];
+var rand = myArray[Math.floor(Math.random() * myArray.length)];
+// console.log (rand);
+gameWord = function () {
+    myArray[Math.floor(Math.random() * myArray.length)];
+}
+
+var gameWord;
+
+guess = event.key
+for i = 0 ....is = word.lenth
+
+//split random word into an array and check each letter to the array
+//rock paper scissors for storing onkeyup
+function splitString(rand, separator) {
+    var arrayOfStrings = rand.split("");
+
+
+var userGuess = document.onkeyup
+//container holding user input from onKeyUp
+
+//establish answerWord from a bank of strings, sring used to compare letters
+
+//listen for input from user onKeyUp, store as var userGuess
+document.getElementById("").onkeyup = function() {myFunction()};
+
+document.onkeyup = function(event) {
+    userText.textContent = event.key;
+  };
+//has letter been guessed? 
+
+//if yes, do nothing
+
+//if no, reduce guesses-left by 1, compare userGuess to letters in the string 
+
+//if letter is part of string, add letter in correct spot to word-blanks
+
+//if not, then add letter to wrong-Guesses
+ 
+//listen for input from user onKeyUp
+
---
+++ assets/javascript/game.js
@@ -0,0 +1,79 @@
+//      VARIABLES       //
+var myArray = ["fire", "crackle", "hiking", "mountain", "desert"];
+
+//choose random word
+var randWord = myArray[Math.floor(Math.random() * myArray.length)]
+
+//globals
+var gameWord;
+var answerArray = [];
+var wrongArray = [];
+var userGuess;
+
+//counters
+var wins = 0;
+var loses = 0;
+var guessesRemain = 10;
+
+// USER PRESSES KEY TO BEGIN GAME; FROM WORD BANK, RANDOM WORD IS SELECTED
+//RANDOM WORD IS STORED TO A GLOBAL VARIABLE; CHARACTER LENGTH OF THE RANDOM WORD IS DETERMINED TO PRINT APPROPRIATE WORD BLANKS
+
+document.onkeyup = function(beginGame) {
+    userGuess = event.key;
+    for (var i = 0; i < randWord.length; i++) {
+        answerArray[i] = "_";
+    }
+    gameWord = answerArray.join(" ");
+    document.getElementById("word-blanks").innerHTML = gameWord;
+
+    function stringSplit() {
+        var answerArray = str.gameWord(" ");};
+
+        if (gameWord.includes(userGuess)) {
+            document.getElementById("wordBlanks").innerHTML = answerArray.push(" ");}
+
+        else { guessesRemain--;
+            document.getElementById("guesses-left").innerHTML = guessesRemain;
+            document.getElementById("wrong-Guesses").innerHTML = userGuess;
+            
+        }
+
+    };
+
+function Checker () {
+    var letter = userGuess; {
+        if (letter.length > 0)
+        {
+            for (var i = 0; i < randWord.length; i++) {
+                if (randWord[i] === letter) {
+                    answerArray[i] = letter;
+                }
+            }
+        guessesRemain--;
+        document.getElementById("guesses-left").innerHTML = guessesRemain;
+        document.getElementById("wordBlanks").innerHTML = answerArray.join(" ");
+}}};
+
+
+// 2. GAME FUNCTION BEGINS AND WAITS FOR KEY PRESS
+
+// 3. AFTER EACH ONKEYUP THE FOLLOWING OCCURS:
+
+    //1. RECORDS THE USED INPUTTED KEY
+
+        //CHECK TO MAKE SURE THE KEY HAS NOT ALREADY BEEN USED
+
+            //IF NOT, STORE LETTTER IN ARRAY
+
+             //IF IT HAS, DONT ADD TO ARRAY AND DECREASE GUESSES REMAINING
+
+    //2. CHECK IF LETTER IS IN THE WORD
+        
+        //FOR LOOP- FOR EACH LETTER IN THE RANDOM WORD:
+
+        //CONDITIONAL: IF THE VALUE OF THE USER CHARACTER MATCHES THE INDEX OF A CHARACTER IN THE WORD, REPLACE BLANK WITH CHARACTER
+
+
+
+// beginGame.onkeyup;
+//         FUNCTIONS           //
