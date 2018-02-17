3ccb413edf62e8c98f86d5df21c9eed4bbdae8ad
Shane Drosi <ShaneDrosi@joshuas-air.attlocal.net>
2018-02-16 19:45:33 -0800

    submit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,12 +3,15 @@ var myArray = ["fire", "crackle", "hiking", "mountain", "desert"];
 
 //choose random word
 var randWord = myArray[Math.floor(Math.random() * myArray.length)]
+var answerArray = [];
 
 //globals
-var gameWord;
-var answerArray = [];
+console.log (answerArray = randWord.split(""));
+var answerArray = randWord.split("");
+var blanks = answerArray;
 var wrongArray = [];
-var userGuess;
+var userGuess = [];
+var progressArray = [];
 
 //counters
 var wins = 0;
@@ -18,43 +21,44 @@ var guessesRemain = 10;
 // USER PRESSES KEY TO BEGIN GAME; FROM WORD BANK, RANDOM WORD IS SELECTED
 //RANDOM WORD IS STORED TO A GLOBAL VARIABLE; CHARACTER LENGTH OF THE RANDOM WORD IS DETERMINED TO PRINT APPROPRIATE WORD BLANKS
 
-document.onkeyup = function(beginGame) {
-    userGuess = event.key;
+//push a letter to begin, store keyed letter event.key into progress array which will be used to compare to answer array 
+document.onkeyup = function(event) {
+    var letter = event.key;
+    console.log(letter);
+    progressArray.push(letter);
+    console.log(progressArray);
+    document.querySelector("#wrong-Guesses").innerHTML = progressArray;
+}
+
+//setup game with a function that will take the random word, scan it for length, replace the elements with that number of underscores.
+//change the element word blanks to match the number of characters in answerArray
+function randomAnswer () {
     for (var i = 0; i < randWord.length; i++) {
-        answerArray[i] = "_";
+    answerArray[i] = "_";
+    answerArray.join(" ");
+    document.getElementById("word-blanks").innerHTML = answerArray;
     }
-    gameWord = answerArray.join(" ");
-    document.getElementById("word-blanks").innerHTML = gameWord;
+//cycle through the progress array and compare each index of progress array to answer array, if the letter matches, replace answer array _ with letter
 
-    function stringSplit() {
-        var answerArray = str.gameWord(" ");};
-
-        if (gameWord.includes(userGuess)) {
-            document.getElementById("wordBlanks").innerHTML = answerArray.push(" ");}
-
-        else {
-            guessesRemain--;
-            document.getElementById("guesses-left").innerHTML = guessesRemain;
-            document.getElementById("wrong-Guesses").innerHTML = userGuess;
-            wrongArray.push(userGuess);
-        }
-
-    };
-
-function Checker () {
-    var letter = userGuess; {
-        if (letter.length > 0)
-        {
-            for (var i = 0; i < randWord.length; i++) {
-                if (randWord[i] === letter) {
-                    answerArray[i] = letter;
-                }
-            }
+    if (answerArray.includes(progressArray[i])) {
+        document.getElementById("wordBlanks").innerHTML = progressArray.push(" ");
+    }
+//if not, then add progress array index to page element wrong-guess, and decrease guessRemain by 1
+    else {
         guessesRemain--;
         document.getElementById("guesses-left").innerHTML = guessesRemain;
-        document.getElementById("wordBlanks").innerHTML = answerArray.join(" ");
-}}};
+        document.getElementById("wrong-Guesses").innerHTML = progressArray;
+    }}
+    
+   // create function that will keep comparing user input to answer array as long as guesses remain is greater than 0
+   //if the letters in the answer array have all been compared and equal letters in progress array, then the user wins
 
+   //once guesses remain equals 0, then prompt game is over
+
+   //create function to reset game and let user know number of wins and losses
+
+//My original logic flow:
+//1. create word array and variables
 
 // 2. GAME FUNCTION BEGINS AND WAITS FOR KEY PRESS
 
@@ -74,7 +78,15 @@ function Checker () {
 
         //CONDITIONAL: IF THE VALUE OF THE USER CHARACTER MATCHES THE INDEX OF A CHARACTER IN THE WORD, REPLACE BLANK WITH CHARACTER
 
+    
+      //I got really lost throughout this exercise. I feel like if I had more time I would have started over. I know I should have approached this from the center and kept better track of my
+      //variables. I wish I had spent more time reviewing exercises and less time banging my head against the computer and continuously trying different options
+      //that honestly didnt quite make sense to me in the first place. I'm taking this homework as a serious learning for how I am going to approach the
+      //bootcamp going forward. I hate failure, and I'm honestly super disappointed.
 
 
-// beginGame.onkeyup;
-//         FUNCTIONS           //
+//Step 1 initialize variables word, random word, put in array, display, etc.
+//Step 2 get user input document.onkeyup
+//Step 3 logic, comparison, conditionals, functions
+//Step 4 wins, losses, in progress
+//Step 5 display output, what letters they guessed, wins, losses
